dd79e9a2c595cbf404ac860c677a87dcf00071ee
Igor Diagilev <103217149+IgorGrrr@users.noreply.github.com>
2023-02-18 13:47:00 +0300

    Fix PEP8

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,6 @@
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers, status, validators
+from rest_framework import serializers, validators
 from rest_framework.validators import UniqueTogetherValidator
 
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
